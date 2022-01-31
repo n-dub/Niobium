4ff927bbb9139df171ab5a38b80eac0768808f90
n-dub <ragucoz@gmail.com>
2022-01-31 14:50:36 +0500

    Fix lexer tests

--- LanguageCore.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ LanguageCore.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using LanguageCore.CodeAnalysis.Syntax;
 using NUnit.Framework;
@@ -70,27 +71,21 @@ namespace LanguageCore.Tests.CodeAnalysis.Syntax
 
         private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
         {
-            return new[]
+            var fixedTokens = Enum.GetValues(typeof(SyntaxKind))
+                .Cast<SyntaxKind>()
+                .Select(k => (kind: k, text: SyntaxFacts.GetText(k)))
+                .Where(t => t.text != null);
+
+
+            var dynamicTokens = new[]
             {
-                (SyntaxKind.PlusToken, "+"),
-                (SyntaxKind.MinusToken, "-"),
-                (SyntaxKind.StarToken, "*"),
-                (SyntaxKind.SlashToken, "/"),
-                (SyntaxKind.BangToken, "!"),
-                (SyntaxKind.EqualsToken, "="),
-                (SyntaxKind.AmpersandAmpersandToken, "&&"),
-                (SyntaxKind.PipePipeToken, "||"),
-                (SyntaxKind.EqualsEqualsToken, "=="),
-                (SyntaxKind.BangEqualsToken, "!="),
-                (SyntaxKind.OpenParenthesisToken, "("),
-                (SyntaxKind.CloseParenthesisToken, ")"),
-                (SyntaxKind.FalseKeyword, "false"),
-                (SyntaxKind.TrueKeyword, "true"),
                 (SyntaxKind.NumberToken, "1"),
                 (SyntaxKind.NumberToken, "123"),
                 (SyntaxKind.IdentifierToken, "a"),
                 (SyntaxKind.IdentifierToken, "abc")
             };
+
+            return fixedTokens.Concat(dynamicTokens);
         }
 
         private static IEnumerable<(SyntaxKind kind, string text)> GetSeparators()
@@ -116,6 +111,11 @@ namespace LanguageCore.Tests.CodeAnalysis.Syntax
             }
 
             if (t1IsKeyword && t2IsKeyword)
+            {
+                return true;
+            }
+
+            if ((t1IsKeyword || t1Kind == SyntaxKind.IdentifierToken) && t2Kind == SyntaxKind.NumberToken)
             {
                 return true;
             }
