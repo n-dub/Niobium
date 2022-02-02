6ad1dedc39fea0290afd8885d79ff0468f6558eb
n-dub <ragucoz@gmail.com>
2022-02-02 14:48:29 +0500

    Support multiple lines in REPL

--- Repl/ReadEvalPrintLoop.cs
+++ Repl/ReadEvalPrintLoop.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using LanguageCore.CodeAnalysis;
 using LanguageCore.CodeAnalysis.Syntax;
+using LanguageCore.CodeAnalysis.Text;
 using Utilities;
 
 namespace Repl
@@ -11,13 +13,14 @@ namespace Repl
     {
         private bool showParseTrees;
         private readonly Dictionary<VariableSymbol, object> variables = new Dictionary<VariableSymbol, object>();
+        private readonly StringBuilder textBuilder = new StringBuilder();
 
         public void Start()
         {
             Console.WriteLine($"Welcome to {LanguageInfo.Name}, v{LanguageInfo.ShortVersion}");
             Console.WriteLine("You can evaluate Niobium expressions and more (type :help or :? to get help).");
 
-            for (var i = 1; ProcessCommand(out var evalResult, i); ++i)
+            for (var commandNumber = 1; ProcessCommand(out var evalResult, ref commandNumber);)
             {
                 if (evalResult != null)
                 {
@@ -26,69 +29,89 @@ namespace Repl
             }
         }
 
-        private bool ProcessCommand(out string evalResult, int commandNumber)
+        private bool ProcessCommand(out string evalResult, ref int commandNumber)
         {
-            Console.Write($"{commandNumber,3}> ");
-            var sourceLine = Console.ReadLine();
+            Console.Write($"{commandNumber++,3}{(textBuilder.Length == 0 ? '>' : '.')} ");
+            
+            var input = Console.ReadLine();
             evalResult = null;
 
-            if (string.IsNullOrEmpty(sourceLine))
+            var isBlank = string.IsNullOrWhiteSpace(input);
+
+            if (input == null)
             {
-                return string.Empty == sourceLine;
+                return false;
             }
 
-            if (sourceLine.First() != ':')
+            if (textBuilder.Length == 0 && input.First() == ':')
             {
-                var syntaxTree = SyntaxTree.Parse(sourceLine);
-                var compilation = new Compilation(syntaxTree);
-                var result = compilation.Evaluate(variables);
+                return ProcessReplCommand(input);
+            }
 
-                if (showParseTrees)
+            textBuilder.AppendLine(input);
+            var sourceLine = textBuilder.ToString();
+            var syntaxTree = SyntaxTree.Parse(sourceLine);
+
+            if (!isBlank && syntaxTree.Diagnostics.Any())
+            {
+                return true;
+            }
+            
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(variables);
+
+            if (showParseTrees)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                syntaxTree.Root.WriteTo(Console.Out);
+                Console.ResetColor();
+            }
+
+            textBuilder.Clear();
+            commandNumber = 1;
+            if (!result.Diagnostics.Any())
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                
+                foreach (var diagnostic in result.Diagnostics)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    syntaxTree.Root.WriteTo(Console.Out);
+                    var lineIndex = syntaxTree.SourceText.GetLineIndex(diagnostic.Span.Start);
+                    var line = syntaxTree.SourceText.Lines[lineIndex];
+                    var lineNumber = lineIndex + 1;
+                    var character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"<repl>:{lineNumber}:{character}: error: " + diagnostic);
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+                    var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+
+                    var prefix = syntaxTree.SourceText.ToString(prefixSpan);
+                    var error = syntaxTree.SourceText.ToString(diagnostic.Span);
+
+                    Console.WriteLine($"{lineNumber,4} | {line}");
+                    Console.Write(new string(' ', 7 + prefix.Length));
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("^" + new string('~', error.Length - 1));
                     Console.ResetColor();
                 }
 
-                if (!result.Diagnostics.Any())
-                {
-                    Console.WriteLine(result);
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    var text = syntaxTree.SourceText;
-
-                    foreach (var diagnostic in result.Diagnostics)
-                    {
-                        var lineIndex = text.GetLineIndex(diagnostic.Span.Start);
-                        var lineNumber = lineIndex + 1;
-                        var character = diagnostic.Span.Start - text.Lines[lineIndex].Start + 1;
-                        
-                        Console.WriteLine();
-
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine($"<repl>:{lineNumber}:{character}: error: " + diagnostic);
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-
-                        var prefix = sourceLine.Substring(0, diagnostic.Span.Start);
-                        var error = sourceLine.Substring(diagnostic.Span.Start, diagnostic.Span.Length);
-
-                        Console.WriteLine(new string(' ', 4) + sourceLine);
-                        Console.Write(new string(' ', 4 + prefix.Length));
-
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine(new string('~', error.Length));
-                        Console.ResetColor();
-                    }
-
-                    Console.WriteLine();
-                }
-
-                return true;
+                Console.WriteLine();
             }
 
+            return true;
+        }
+
+        private bool ProcessReplCommand(string sourceLine)
+        {
             switch (sourceLine)
             {
                 case ":help":
@@ -99,19 +122,17 @@ namespace Repl
    :help, :?                    Show this message with the list of commands
    :show-parse-tree             Toggle showing parse tree of last expression
    :quit                        Exit the REPL");
-                    break;
+                    return true;
                 case ":show-parse-tree":
                     showParseTrees = !showParseTrees;
                     Console.WriteLine(showParseTrees ? "Showing parse trees." : "Not showing parse trees");
-                    break;
+                    return true;
                 case ":quit":
                     return false;
                 default:
                     Console.WriteLine($"Invalid REPL command: {sourceLine}");
-                    break;
+                    return true;
             }
-
-            return true;
         }
     }
 }
