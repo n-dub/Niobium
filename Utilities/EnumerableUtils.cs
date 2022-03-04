7b513f260064031375ce0a665b2ecb8704c4746b
n-dub <ragucoz@gmail.com>
2022-03-04 11:51:55 +0500

    Added dictionary deconstruction utility

---
+++ Utilities/EnumerableUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities
+{
+    public static class EnumerableUtils
+    {
+        public static IEnumerable<(TKey, TValue)> Deconstruct<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> d)
+        {
+            return d.Select(x => (x.Key, x.Value));
+        }
+    }
+}
